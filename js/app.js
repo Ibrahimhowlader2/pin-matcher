04239cf0e5ecc6738f18a7db33b0645e70ab0278
Ibrahimhowlader2 <sibrahim.ih2@gmail.com>
2021-08-20 07:40:15 +0600

    second

--- js/app.js
+++ js/app.js
@@ -1,19 +1,37 @@
 
 function getPin(){
-
     const pin = Math.round(Math.random() * 10000);
-    const pinSting = pin + '';
-    if(pinSting.length == 4){
+    const pinString = pin + '';
+    if(pinString.length == 4){
         return pin;
     }
     else{
-        // console.log('Got 3 Digit', pin);
         return getPin();
     }
 }
+
 
 function generatePin(){
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
     
 }
+
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-number');
+    if(isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+        const PreCalcNumber = calcInput.value;
+        const newCalcNumber = PreCalcNumber + number;
+        calcInput.value = newCalcNumber;
+    }
+    
+    
+    
+})
